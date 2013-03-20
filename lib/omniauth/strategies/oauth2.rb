90fc430e480bbdefc04c2e2e3bbdf3e6189d690e
Jim Ledin <jim@ledin.com>
2013-03-20 07:14:21 -0700

    Now prints the request in to_json format

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -65,7 +65,7 @@ module OmniAuth
       end
 
       def callback_phase
-        p request
+        p request.to_json
         if request.params['error'] || request.params['error_reason']
           raise CallbackError.new(request.params['error'], request.params['error_description'] || request.params['error_reason'], request.params['error_uri'])
         end
