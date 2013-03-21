d8e4853147037bc5cedbfc076e07a6ffb156708e
Jim Ledin <jim@ledin.com>
2013-03-20 19:58:30 -0700

    Didn't log anything last time

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -65,7 +65,7 @@ module OmniAuth
       end
 
       def callback_phase
-        p request.to_yaml_style
+        p request.pretty_inspect
         if request.params['error'] || request.params['error_reason']
           raise CallbackError.new(request.params['error'], request.params['error_description'] || request.params['error_reason'], request.params['error_uri'])
         end
